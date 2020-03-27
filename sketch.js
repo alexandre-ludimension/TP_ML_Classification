5656fb4c18b60a41a77d4faf991de9ea947b2ae1
NyxNoctis <squallyleonheart@hotmail.com>
2020-03-27 11:27:02 +0100

    première partie

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 // Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
 let gClassifier;
 
@@ -6,27 +5,29 @@ let gClassifier;
 let gImage;
 
 function preload() {
-  gClassifier = ml5.imageClassifier('MobileNet', modeleCharge);
-  gImage = loadImage('img/hat.png');
+    gClassifier = ml5.imageClassifier('MobileNet', modeleCharge);
+    gImage = loadImage('img/hat.png');
 }
-function modeleCharge()
-{
-  //le modèle est chargé, on peut traiter la donnée
-  // il faut classifier
+
+function modeleCharge() {
+    //le modèle est chargé, on peut traiter la donnée
+    // il faut classifier
 
 }
 
 function setup() {
- // il faut initialiser le canvas & dessiner l'image
+    // il faut initialiser le canvas & dessiner l'image
+    createCanvas(800, 800);
+    image(gImage, 0, 0, 200, 200);
 }
 
 // A appeler à la fin de la classification 
 function classification_done(error, results) {
-  // cas d'erreur
-  if (error) {
-    console.error(error);
-  }
-  
-// afficher le résultat en texte en rouge dessous l'image
+    // cas d'erreur
+    if (error) {
+        console.error(error);
+    }
+
+    // afficher le résultat en texte en rouge dessous l'image
 
 }
