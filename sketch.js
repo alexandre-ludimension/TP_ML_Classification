f11c4ea1202a7e8574cb7e4c78a1fa7fb2a5044d
Guillaume.P <pontoiseguillaume@gmail.com>
2020-03-27 12:29:36 +0100

    Avancée et fonctionnement du projet

--- sketch.js
+++ sketch.js
@@ -20,6 +20,7 @@ function setup() {
  // il faut initialiser le canvas & dessiner l'image
  createCanvas(windowWidth, windowHeight);
  image(gImage, 0, 0);
+ modeleCharge();
 }
 
 // A appeler à la fin de la classification 
@@ -27,6 +28,17 @@ function classification_done(error, results) {
   // cas d'erreur
   if (error) {
     console.error(error);
+  }else{
+    let lTextSize = 20;
+    fill(255,0,0);
+    textSize(lTextSize);
+    text('Objet trouvé = ' + results[0].label, 0, height-500);
+    text('Confidence = ' + results[0].confidence, 0, height-470);
+    text('Objet trouvé = ' + results[1].label, 0, height-400);
+    text('Confidence = ' + results[1].confidence, 0, height-370);
+    text('Objet trouvé = ' + results[2].label, 0, height-300);
+    text('Confidence = ' + results[2].confidence, 0, height-270);
+    console.log(results);
   }
   
 // afficher le résultat en texte en rouge dessous l'image
