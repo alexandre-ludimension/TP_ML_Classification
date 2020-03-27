da922a8889f4153c0ef4db03c66f5d0e3381eb08
Alexandre <alexandre@ludimension.com>
2020-03-27 08:59:03 +0100

    [FCT] Ajout du squelette pour le TP Mobile NET

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+let gClassifier;
+
+// A variable to hold the image we want to classify
+let gImage;
+
+function preload() {
+  gClassifier = ml5.imageClassifier('MobileNet', modeleCharge);
+  gImage = loadImage('img/hat.png');
+}
+function modeleCharge()
+{
+  //le modèle est chargé, on peut traiter la donnée
+  // il faut classifier
+
+}
+
+function setup() {
+ // il faut initialiser le canvas & dessiner l'image
+}
+
+// A appeler à la fin de la classification 
+function classification_done(error, results) {
+  // cas d'erreur
+  if (error) {
+    console.error(error);
+  }
+  
+// afficher le résultat en texte en rouge dessous l'image
+
+}
