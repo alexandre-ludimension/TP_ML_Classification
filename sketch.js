f48016441cb2d36d8aee9931fbe18525f8dc192f
POMIES <emiliepomies21@gmail.com>
2020-03-27 11:43:07 +0100

    [FCT] modification

--- sketch.js
+++ sketch.js
@@ -9,15 +9,27 @@ function preload() {
   gClassifier = ml5.imageClassifier('MobileNet', modeleCharge);
   gImage = loadImage('img/hat.png');
 }
+
 function modeleCharge()
 {
   //le modèle est chargé, on peut traiter la donnée
+  console.log('Model Loaded!');
   // il faut classifier
-
+  gClassifier.classify(gImage, classification_done);
+  
 }
+  
+
 
 function setup() {
  // il faut initialiser le canvas & dessiner l'image
+
+  createCanvas(800, 800);
+  background(0);
+  image(gImage,0,0,200,200);
+
+  return modeleCharge();
+
 }
 
 // A appeler à la fin de la classification 
@@ -26,7 +38,16 @@ function classification_done(error, results) {
   if (error) {
     console.error(error);
   }
+
   
 // afficher le résultat en texte en rouge dessous l'image
 
+  console.log(results);
+  let label = results[0].className;
+  let prob = results[0].probability;
+  fill(255,0,0);
+  textSize(32);
+  text(label,10, height -100);
+  createP(label);
+  createP(prob);
 }
